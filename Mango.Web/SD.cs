dcc622c622927486d7a5429071c2d14f7bbb9db3
Lucas <lucasrocini@hotmail.com>
2021-08-03 21:47:22 -0300

    Created ProductDto and Repository

---
+++ Mango.Web/SD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web
+{
+    public static class SD //SD = Static Details
+    {
+        public static string ProductAPIBase { get; set; }
+        public enum ApiType { 
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
