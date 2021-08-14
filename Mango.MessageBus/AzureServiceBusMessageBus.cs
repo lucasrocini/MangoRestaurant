fb9db415149a078a0518ba04889e67d2c05199d6
Lucas <lucasrocini@hotmail.com>
2021-08-14 17:19:41 -0300

    Created Integration for Azure Service Bus

---
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,41 @@
+﻿
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class AzureServiceBusMessageBus : IMessageBus
+    {
+        //can be improved
+        private string connectionString = "Endpoint=sb://mangorestaurantlar.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=stW2XNw1fdBzfuldnziIkkdQlhD++Ggyb2Ut+av3988=";
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topicName);
+            //OLD VERSION - Microsoft.Azure.ServiceBus
+            //ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            //OLD VERSION - Microsoft.Azure.ServiceBus
+            //var finalMessage = new Message(Encoding.UTF8.GetBytes(jsonMessage))
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            //OLD VERSION - Microsoft.Azure.ServiceBus
+            //await senderClient.SendAsync(finalMessage);
+            //await senderClient.CloseAsync();
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
